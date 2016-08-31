80d83e70da000b421742554d2889b34e619815cb
ManojParmar-BTC <manoj.parmar@botreetechnologies.com>
2016-08-31 12:28:41 +0530

    displaying repo inforation complete

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -4,15 +4,21 @@ class RepositoriesController < ApplicationController
   end
 
   def index
-    @repository = response_from_uri("https://api.github.com/repos/Naiya123/app1")
+    @repository = response_from_uri("https://api.github.com/repos/rails/rails")
     @language = response_from_uri(@repository[:languages_url])
     @total = @language.values.sum
+    @user = author_info('rails')
   end
 
+  private
   def response_from_uri(uri)
     uri = URI(uri)
     http_response = Net::HTTP.get_response(uri)
     response = JSON(http_response.body).symbolize_keys
     response
+  end
+
+  def author_info(user_name)
+    user_info = response_from_uri("https://api.github.com/users/#{user_name}")
   end
 end
