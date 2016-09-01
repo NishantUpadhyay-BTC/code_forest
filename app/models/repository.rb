62daa7cf3964b305641878f50bc8586e2057ff5f
ManojParmar-BTC <manoj.parmar@botreetechnologies.com>
2016-09-01 19:05:24 +0530

    repository form with design(WIP)

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,6 +1,6 @@
 class Repository < ApplicationRecord
   has_many :languages, inverse_of: :repository
-    has_attached_file :poc_image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/public/missing.png",
+    has_attached_file :poc_image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "demo_repo.png",
                       url: "/system/:id/:style/:filename"
   accepts_nested_attributes_for :languages
   validates_attachment_content_type :poc_image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
