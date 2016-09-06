b2fc4fae751c41f08d4eb8e0873038a96b438255
ManojParmar-BTC <manoj.parmar@botreetechnologies.com>
2016-09-06 12:17:21 +0530

    stable wroking

--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 20160905081018) do
     t.index ["user_id"], name: "index_favourites_on_user_id", using: :btree
   end
 
+  create_table "github_responses", force: :cascade do |t|
+    t.string   "url"
+    t.string   "response"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "impressions", force: :cascade do |t|
     t.string   "impressionable_type"
     t.integer  "impressionable_id"
