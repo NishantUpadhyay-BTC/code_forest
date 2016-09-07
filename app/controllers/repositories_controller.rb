ebcdc872e74a0caa170a9359db2d2fa9f4275b30
NaiyaShah-BTC <naiya.shah@botreetechnologies.com>
2016-09-07 15:57:57 +0530

    Service pattern implemented for Github api calls

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -9,7 +9,7 @@ class RepositoriesController < ApplicationController
   end
 
   def new
-    repository_values_result = repository_values(params[:user_name], params[:repo_name])
+    repository_values_result = Github::FetchRepo.repository_values(params[:user_name], params[:repo_name])
     @repository = Repository.new(repository_values_result[:repository_details])
 
     repository_values_result[:language].each do |language,code|
@@ -64,67 +64,6 @@ class RepositoriesController < ApplicationController
   end
 
   private
-  def response_from_uri(uri)
-    uri = URI(uri)
-    http_response = Net::HTTP.get_response(uri)
-    response = JSON(http_response.body).symbolize_keys
-    response
-  end
-
-  #returns authors infor from github id.
-  def author_info(user_name)
-    user_info = response_from_uri("https://api.github.com/users/#{user_name}")
-  end
-
-  #returns repository details for initializing @repository object in new action
-  #returns hash of languages used in that repository
-  def repository_values(user_name, repository_name)
-    # repository = response_from_uri("https://api.github.com/repos/#{user_name}/#{repository_name}")
-    # language = response_from_uri(repository[:languages_url])
-    # user = author_info(user_name)
-
-    # repository_details = {
-    #   author_name: user[:login],
-    #   avatar_url: user[:avatar_url],
-    #   repo_id: repository[:id],
-    #   name: repository[:name],
-    #   description: repository[:description],
-    #   private: repository[:private],
-    #   download_link: "https://github.com/#{repository[:full_name]}/archive/#{repository[:default_branch]}.zip",
-    #   clone_url: repository[:clone_url],
-    #   git_url: repository[:git_url],
-    #   ssh_url: repository[:ssh_url],
-    #   svn_url: repository[:svn_url],
-    #   no_of_stars: repository[:stargazers_count],
-    #   no_of_watchers: repository[:forks],
-    #   has_wiki: repository[:has_wiki],
-    #   wiki_url: "http://github.com/#{repository[:full_name]}/wiki",
-    #   repo_created_at: repository[:created_at],
-    #   last_updated_at: repository[:updated_at]
-    #   }
-
-    # {repository_details: repository_details, language: language}
-
-    {:repository_details=>
-  {:author_name=>"NishantUpadhyay-BTC",
-   :avatar_url=>"https://avatars.githubusercontent.com/u/6542029?v=3",
-   :repo_id=>66931911,
-   :name=>"code_forest",
-   :description=>"Largest collection of demo projects and proof of concepts",
-   :private=>false,
-   :download_link=>"https://github.com/NishantUpadhyay-BTC/code_forest/archive/master.zip",
-   :clone_url=>"https://github.com/NishantUpadhyay-BTC/code_forest.git",
-   :git_url=>"git://github.com/NishantUpadhyay-BTC/code_forest.git",
-   :ssh_url=>"git@github.com:NishantUpadhyay-BTC/code_forest.git",
-   :svn_url=>"https://github.com/NishantUpadhyay-BTC/code_forest",
-   :no_of_stars=>1,
-   :no_of_watchers=>0,
-   :has_wiki=>true,
-   :wiki_url=>"http://github.com/NishantUpadhyay-BTC/code_forest/wiki",
-   :repo_created_at=>"2016-08-30T10:43:54Z",
-   :last_updated_at=>"2016-09-02T06:06:01Z"},
- :language=>{:Ruby=>24670, :HTML=>5154, :JavaScript=>1201, :CSS=>736}}
-  end
 
   def repository_params
     params.require(:repository).permit(:id, :author_name, :avatar_url, :repo_id, :name, :description, :private,
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,22 +3,11 @@ class UsersController < ApplicationController
   end
 
   def index
-    @repositories = response_from_uri("https://api.github.com/users/#{current_user.name}/repos")
+    @repositories = Github::FetchUser.call_user_repos(current_user.name)
   end
 
   def show
     @pocs = Repository.where(author_name: current_user.name)
-    @repositories = response_from_uri("https://api.github.com/users/#{current_user.name}/repos")
-  end
-
-  def destory
-    redirect_to :back
-  end
-  
-  def response_from_uri(uri)
-    uri = URI(uri)
-    http_response = Net::HTTP.get_response(uri)
-    response = JSON(http_response.body)
-    response
+    @repositories = Github::FetchUser.call_user_repos(current_user.name)
   end
 end
