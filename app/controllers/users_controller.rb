099a1476d31828c1eb4e1a4f4fe7b748e2e94225
ManojParmar-BTC <manoj.parmar@botreetechnologies.com>
2016-09-19 13:38:57 +0530

    pagination added for poc table

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
-    @pocs = Repository.where(author_name: current_user.name)
+    @pocs = Repository.where(author_name: current_user.name).paginate(page: params[:poc_page])
     @repositories = Github::FetchAllRepos.new(current_user.name, session[:github_token]).call
   end
 end
