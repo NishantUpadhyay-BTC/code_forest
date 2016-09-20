2f138be39492238cf9d7feb37e154b8b9df48e0f
ManojParmar-BTC <manoj.parmar@botreetechnologies.com>
2016-09-20 14:49:01 +0530

    sorting complete for poc table

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,23 @@
 require 'will_paginate/array'
 class UsersController < ApplicationController
   def show
-    @pocs = Repository.where(author_name: current_user.name).paginate(page: params[:poc_page]).order( "#{params[:column]} #{params[:sorting_order]}" )
+    @pocs = Repository.where(author_name: current_user.name)
+    @pocs = ['impressions', 'favourites'].include?(params[:column]) ?
+           sort_with_association(@pocs)  :
+           @pocs.order("#{params[:column]} #{params[:poc_sorting]}")
     @repositories = Github::FetchAllRepos.new(current_user.name, session[:github_token]).call
     @repositories = filter_pocs(@repositories).paginate(per_page: 1, page: params[:repo_page])
+    @pocs = @pocs.paginate(page: params[:poc_page])
     respond_to do |format|
         format.html
         format.js
     end
+  end
+
+  private
+  def sort_with_association(pocs)
+    params[:poc_sorting] == "ASC" ?
+    pocs = pocs.sort_by { |repo| repo.send(params[:column]).count } :
+    pocs = pocs.sort_by { |repo| repo.send(params[:column]).count }.reverse
   end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
-  def sorting_link(column)
-    sorting_order = (params[:column] == column) ? toggle_order(params[:sorting_order]) : "ASC"
-    link_to(sorting_order, user_path(params[:id], column: column, sorting_order: sorting_order), remote: true)
+  def sorting_link(column, table)
+    sorting_order = (params[:column] == column) ? toggle_order(params["#{table}_sorting"]) : "ASC"
+    link_to(sorting_order, user_path(params[:id], column: column, "#{table}_sorting" => sorting_order), remote: true)
   end
 
   def toggle_order(order)
