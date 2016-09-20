fd7ad98b6bbafad6487f63a01f1d73a0b7c68669
NaiyaShah-BTC <naiya.shah@botreetechnologies.com>
2016-09-20 19:27:16 +0530

    necessary change for credentials

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,19 +29,7 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.perform_caching = true
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.delivery_method = :smtp
-
-  config.action_mailer.smtp_settings = {
-  address:              'smtp.gmail.com',
-  port:                 '587',
-  domain:               'gmail.com',
-  :user_name          => 'codeforestdemo@gmail.com',
-  :password           => 'india@botree',
-  authentication:       'login',
-  enable_starttls_auto: true
-  }
+  config.action_mailer.perform_caching = flase
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
