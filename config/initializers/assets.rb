890dfca2c03f84b42c58324b246327bc1a6d0783
NaiyaShah-BTC <naiya.shah@botreetechnologies.com>
2016-09-26 14:23:06 +0530

    Theme changes

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,7 +7,5 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 
 # Precompile additional assets.
-Rails.application.config.assets.precompile =  ['*.js']
-Rails.application.config.assets.precompile += %w(*.js, *.css)
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
