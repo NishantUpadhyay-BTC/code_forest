1b0f2fc9fdc2407a51a52f3d53eeefa269cb1fd6
ManojParmar-BTC <manoj.parmar@botreetechnologies.com>
2016-09-30 14:33:27 +0530

    pagination for 3 tables

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def pagination
+    {class: "pagination center-align", previous_label: "<i class='material-icons'>chevron_left</i>".html_safe,
+    next_label: "<i class='material-icons'>chevron_right</i>".html_safe}
+  end
 end
